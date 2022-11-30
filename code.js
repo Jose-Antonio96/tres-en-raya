fd644f325c73e2aaa39529159d8f6c4c9249e8c0
JoseAPR <92516246+Jose-Antonio96@users.noreply.github.com>
2022-11-30 17:39:32 +0100

    Creación del espacio de juego

--- code.js
+++ code.js
@@ -1,9 +1,13 @@
 
-let tablero = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0]
-  ];
+// Función que permite cerrar el menú de instrucciones.
+function close(){
+    document.getElementById("Instructions").style.visibility = "hidden";
+}
+
+document.querySelector('button').forEach(
+    obj => obj.addEventListener("click", btnPulsado));
+
+const btnPulsado = (e) =>{
+    const btn = e.target;
+    btn.stye.backgroundColor = turno %2 ? 'blue' : 'red'; 
+}
