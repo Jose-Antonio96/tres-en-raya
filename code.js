6a89ccebb129f0ea730f135eb486b6287ae94fd2
JoseAPR <92516246+Jose-Antonio96@users.noreply.github.com>
2022-12-06 20:44:59 +0100

    Falta arreglar la función que nos da victoria

--- code.js
+++ code.js
@@ -4,20 +4,23 @@
 // y en ese momento mostrará el tiempo invertido en total por cada jugador entre todos sus turnos
 
 
-let turn = true;
 
 const boxList = document.querySelectorAll('.table .box')
-boxList.forEach(function(box, index){
-    box.textContent = index
-    box.addEventListener('click', function(){
-        if(turn){
-            box.style.setProperty('background-image' , 'url(img/circulo.png')
-        }else{
-            box.style.setProperty('background-image' , 'url(img/cruz.png')
+boxList.forEach(function (box) {
+    box.addEventListener('click', function () {
+        if (turn) {
+            box.classList.add('mark-x')
+        } else {
+            box.classList.add('mark-o')
+        }
+        const winner = victory()
+        if(winner = true){
+            alert("Winner!")
         }
         turn = !turn;
-    })
+    }, {once: true})
 })
+
 function DisplayTurn(){
 
 }
@@ -39,25 +42,27 @@ function victory(){
             return true;
     }else if
         (  table[6] == table[7] && table[6] == table[8] && table[6]){
-        return true;
+            return true;
     }else if
         (table[0] == table[3] && table[0] == table[6] && table[0]){
-        return true;
+            return true;
     }else if
         (table[1] == table[4] && table[1] == table[7] && table[1]){
-        return true;
+            return true;
     }else if
         (table[2] == table[5] && table[2] == table[8] && table[2]){
-        return true;
+            return true;
     }else if
         (table[0] == table[4] && table[0] == table[8] && table[0]){
-        return true;
+            return true;
     }else if
         (table[2] == table[4] && table[2] == table[6] && table[2]){
-        return true;
+            return true;
     }
     return false;
  
 }
+
+
 
 window.onload= ShowTable();
